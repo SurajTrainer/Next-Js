2407be6df09bb1754f792ae1168003794b76445e
Suraj Singh <139257270+SurajTrainer@users.noreply.github.com>
2024-05-22 18:05:09 +0530

    price navbar

---
+++ next app/app/price/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Script from 'next/script'
+
+const Price = () => {
+  return (
+  <>
+    <Script>{`alert("price is too much high...")`}</Script>
+  </>
+  )
+}
+
+export default Price
